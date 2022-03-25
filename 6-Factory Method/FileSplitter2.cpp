c5d7ba762f7ca8838728bfcceff224b0aeeb3f6d
taolijie <taolijie11111@gmail.com>
2022-03-25 18:21:24 +0800

    add files

---
+++ 6-Factory Method/FileSplitter2.cpp
@@ -0,0 +1,51 @@
+/*
+为了解决紧耦合的问题
+*/
+
+//具体类
+class BinarySplitter : public ISplitter{
+    
+};
+
+class TxtSplitter: public ISplitter{
+    
+};
+
+class PictureSplitter: public ISplitter{
+    
+};
+
+class VideoSplitter: public ISplitter{
+    
+};
+
+//具体工厂
+class BinarySplitterFactory: public SplitterFactory{
+public:
+    virtual ISplitter* CreateSplitter(){
+        return new BinarySplitter();//不能写成return new ISplitter();
+        //虚函数是一种运行依赖
+    }
+};
+
+class TxtSplitterFactory: public SplitterFactory{
+public:
+    virtual ISplitter* CreateSplitter(){
+        return new TxtSplitter();
+    }
+};
+
+class PictureSplitterFactory: public SplitterFactory{
+public:
+    virtual ISplitter* CreateSplitter(){
+        return new PictureSplitter();
+    }
+};
+
+class VideoSplitterFactory: public SplitterFactory{
+public:
+    virtual ISplitter* CreateSplitter(){
+        return new VideoSplitter();
+    }
+};
+
